aa7c35a11c2d538dfa4e907bfc9f8b23a720ebd6
johnmckenzie <johnmckenzie@labpc061.mshome.net>
2018-02-19 14:54:47 +0000

    Deleted MYSQLConnection class

---
+++ test/agile/computing/group/pkg8/DBConnectionTest.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agile.computing.group.pkg8;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author vincentnieutin
+ */
+public class DBConnectionTest {
+
+    DBConnection connection;
+    String host = "sql2.freesqldatabase.com";
+    String db = "sql2220155";
+    String username = "sql2220155";
+    String password = "nC8!uE3*";
+
+    public DBConnectionTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        connection = new DBConnection(host, db, username, password);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of connect method, of class Connexion.
+     */
+    @Test
+    public void testConnect() throws Exception {
+        boolean expResult = true;
+        boolean result = connection.open(host, db, username, password);
+        assertEquals("Connexion failed", expResult, result);
+    }
+
+    /**
+     * Test of disconnect method, of class DBConnection.
+     */
+    @Test
+    public void testDisconnect() throws Exception {
+        connection.open(host, db, username, password);
+        boolean expResult = false;
+        boolean result = connection.close();
+        assertEquals("Failed to close the connection", expResult, result);
+    }
+
+}
