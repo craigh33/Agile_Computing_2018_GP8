59e093f35e23ec0e1a02e19dfc262d58e3caff53
vincentnieutin <vincentnieutin@labpc063.mshome.net>
2018-02-22 16:12:07 +0000

    New Project Implemented Upload Implemented

--- src/agile/computing/group/pkg8/DBConnection.java
+++ src/agile/computing/group/pkg8/DBConnection.java
@@ -5,12 +5,16 @@
  */
 package agile.computing.group.pkg8;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Hashtable;
+
 /**
  *
  * @author vincentnieutin
@@ -20,14 +24,14 @@ public class DBConnection {
     Connection con;
     String host, db, username, password;
     Statement stmt;
-   
+
     public DBConnection(String host, String db, String username, String password) {
         this.host = host;
         this.db = db;
         this.username = username;
         this.password = password;
         try {
-            con = DriverManager.getConnection("jdbc:mysql://" + host + "/"+db+"?user=" + username + "&password=" + password);
+            con = DriverManager.getConnection("jdbc:mysql://" + host + "/" + db + "?user=" + username + "&password=" + password);
         } catch (SQLException e) {
             // change to GUI output stream as required
             e.printStackTrace(System.out);
@@ -49,17 +53,17 @@ public class DBConnection {
     public String getDatabase() {
         return this.db;
     }
-    
-    public Hashtable getUserByStaffID(String staffID){
+
+    public Hashtable getUserByStaffID(String staffID) {
         /*
         returns hashtable with keys equal to database column names as result
-        */
-        Hashtable<String,String> result = new Hashtable();
-        try{
+         */
+        Hashtable<String, String> result = new Hashtable();
+        try {
             stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM Staff WHERE StaffID = "+staffID);
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Staff WHERE StaffID = " + staffID);
             //return rs;
-            while(rs.next()){
+            while (rs.next()) {
                 System.out.println(rs.getString("StaffID"));
                 result.put("StaffID", rs.getString("StaffID"));
                 result.put("Password", rs.getString("Password"));
@@ -68,11 +72,40 @@ public class DBConnection {
                 result.put("email", rs.getString("email"));
                 result.put("JobType", rs.getString("JobType"));
             }
-        }
-        catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace(System.out);
         }
         return result;
-        
+
+    }
+
+    public ResultSet getProjects() {
+        ResultSet rs = null;
+
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM 17agileteam8db.project");
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
+
+        return rs;
+    }
+
+    public void newProject(int id, String projectName, String researcher, String comments, String fileName, String filePath) {
+        String date = new SimpleDateFormat("yyyyMMdd").format(new Date());
+        try {
+            File destination;
+            String newName = id + "" + new SimpleDateFormat("ddMMyyhhmmss").format(new Date());
+            String format = fileName.split("\\.")[1];
+
+            destination = new File(filePath + "\\" + newName + "." + format);
+
+            stmt = con.createStatement();
+            stmt.execute("INSERT INTO project (name, researcher, comments ,date, file_name,file_path) VALUES ('" + projectName + "','" + researcher + "','" + comments + "','" + date + "','" + newName + "." + format + "', '" + filePath.replace("\\", "\\\\") + "\\\\" + newName + "." + format + "')");
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
+
     }
 }
